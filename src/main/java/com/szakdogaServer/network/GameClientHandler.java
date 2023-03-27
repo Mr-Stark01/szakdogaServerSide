1d673caba41423bba2f7817921887afcc377aa32
Stark <fs1t3k@inf.elte.hu>
2023-03-27 12:54:56 +0200

    remodel so server does everything that's not directly to do with graphics(only pathfinding redone)

--- src/main/java/com/szakdogaServer/network/GameClientHandler.java
+++ src/main/java/com/szakdogaServer/network/GameClientHandler.java
@@ -1,6 +1,7 @@
 package com.szakdogaServer.network;
 
-import com.szakdogaServer.network.DTO.*;
+import com.szakdogaServer.BusinessLogic.ServerLogic;
+import org.datatransferobject.DTO;
 
 import java.io.IOException;
 import java.io.ObjectInputStream;
@@ -8,16 +9,23 @@ import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.Callable;
+import java.util.concurrent.Future;
 
 public class GameClientHandler implements Callable<Integer> {//TODO tesztelés tényleges androidon futó alkalmazás nélkül setupolni valamit
     private final Socket clientSocket;
     private static ObjectOutputStream objectOutputStream;
     private DTO dto;
+    private List<DTO> DTOListOut = new ArrayList<>();
+    private BlockingQueue<DTO>  blockingQueueOut;
+    private BlockingQueue<ArrayList<DTO>> blockingQueueIn;
     private static ObjectInputStream objectInputStream;
     //TODO blocking quet olvasni csak és onnan sendData
-    public GameClientHandler(Socket socket) throws InterruptedException {
+    public GameClientHandler(Socket socket, BlockingQueue<DTO> blockingQueueOut,BlockingQueue<ArrayList<DTO>> blockingQueueIn) throws InterruptedException {
         this.clientSocket = socket;
+        this.blockingQueueOut = blockingQueueOut;
         try {
             objectOutputStream = new ObjectOutputStream(clientSocket.getOutputStream());//Needs to be created first
             objectInputStream = new ObjectInputStream(clientSocket.getInputStream());
@@ -31,9 +39,8 @@ public class GameClientHandler implements Callable<Integer> {//TODO tesztelés t
         while(true){//TODO szétszedni kétfelé
             try {
                 receiveData();
-                calculate(dto);
+                blockingQueueOut.add(dto);
                 sendData();
-
             }
             catch (ClassNotFoundException e){
                 //e.printStackTrace();
@@ -43,16 +50,19 @@ public class GameClientHandler implements Callable<Integer> {//TODO tesztelés t
             }
         }
     }
-    public ArrayList receiveData() throws IOException, ClassNotFoundException {//TODO csak deseralizal és berakja blocking queue ba
-
-        DTO dto  = (DTO) objectInputStream.readObject();
-        //DTO dto2 = (DTO) objectInputStream.readObject();//TODO csak adja ki a másik osztálynak + küllön szálra kiteni
-        return null;
+    public void receiveData() throws IOException, ClassNotFoundException {//TODO csak deseralizal és berakja blocking queue ba
+        DTO dto  = (DTO) objectInputStream.readObject(); //TODO csak adja ki a másik osztálynak + küllön szálra kiteni
     }// TODO id alapján lehet kiolvasni esetleg converter osztály felismeri azt is hogy pontosan milyen adat érkezet és lekezelni
     public void sendData() throws IOException {//TODO csak seralizel és kiolvasa blocking queue ba
-        objectOutputStream.writeObject(new Date()); // TODO same here
-    }
-    public DTO getDto(){
-        return null;
+        try{
+            DTOListOut=blockingQueueIn.take();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        for(DTO dto:DTOListOut){
+            objectOutputStream.writeObject(dto);
+        }
+        DTOListOut=null;
+        dto = null;
     }
 }
